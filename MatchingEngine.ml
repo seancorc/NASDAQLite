52b0a870d16fd554949eb2f091b9132389fb2504
Aaron Buchwald <aaron.buchwald56@gmail.com>
2019-11-06 16:24:20 -0500

    Remove unnecessary printing

--- MatchingEngine.ml
+++ MatchingEngine.ml
@@ -12,13 +12,10 @@ module MatchingEngine = struct
   let matchorder obook order =
     match OrderBook.get_complement_order order obook with 
     | None -> 
-      let _ = print_int (OrderBook.num_buys (OrderBook.insert order obook)) in
       [], (OrderBook.insert order obook)
     | Some o ->
       let transaction = [(order, o)] in 
-      let _ = print_int (List.length transaction) in 
       let first_removed = (OrderBook.remove order obook) in
       let final_book = OrderBook.remove o first_removed in 
-      let _ = print_int (OrderBook.num_buys final_book) in 
       transaction, final_book
 end
