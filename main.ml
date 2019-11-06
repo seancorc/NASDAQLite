0e91b1c3b18daf52af103255b2b788228364d9bf
Jack Rehmann <jcr285@cornell.edu>
2019-11-06 10:28:04 -0500

    Implemented more of the UI.

--- main.ml
+++ main.ml
@@ -1,11 +1,14 @@
 
+open AccountManager
+open Account
+
 type action = Login | Signup 
 
 (** [start_loop] is the eithier the Login or Signup action depending on the 
     user's input*)
 let rec start_loop () = 
   print_endline ("Type 'login' to login or 'signup' to signup");
-  match read_line () with
+  match String.trim(read_line ()) with
   | "login" -> Login
   | "signup" -> Signup
   | _ -> print_endline "I couldn't understand that command, please try again."; 
@@ -16,16 +19,43 @@ let startup_action () =
   print_endline ("Welcome to NASDAQLite!"); 
   start_loop ()
 
-let login () = 
+let rec run a m = 
+  print_endline ("Account Username: " ^ Account.username a);
+  print_endline "To log out of this account, type 'logout'";
+  let i = (read_line ()) in 
+  match String.trim i with 
+  | "logout" -> restart m
+
+
+and login m = 
   print_string "Username: ";
-  let input = (read_line ()) in 
-  ()
+  let username = String.trim(read_line ()) in 
+  print_string "Password: ";
+  let password = (read_line ()) in 
+  try run (AccountManager.login m username password) m with 
+  | InvalidPassword -> print_endline "Incorrect Password"; login m
+  | (InvalidUsername s) -> print_endline s; login m
 
 
-let main () =
+and register m = 
+  print_string "Username: ";
+  let username = String.trim(read_line ()) in 
+  print_string "Password: ";
+  let password = (read_line ()) in 
+  let new_account = AccountManager.register m username password in 
+  run new_account m
+
+and restart m = 
+  match start_loop () with 
+  | Login -> login m
+  | Signup -> register m
+
+and main () =
+  let m = AccountManager.create () in
   match startup_action () with 
-  | Login -> login ()
-  | Signup -> failwith "unimplemented"
+  | Login -> login m
+  | Signup -> register m
+
 
 let () = main ()
 
