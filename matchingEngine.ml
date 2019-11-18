48f90d73ba6d30a1654d4a55b34f947f6e325c5d
Aaron Buchwald <aaron.buchwald56@gmail.com>
2019-11-18 00:06:09 -0500

    Update order book and start matching engine changes

--- matchingEngine.ml
+++ matchingEngine.ml
@@ -1,14 +1,111 @@
 open OrderBook
-
-
-type transaction = order * order 
+open AccountManager
 
 module type MatchingEngine = sig 
   val matchorder: OrderBook.t -> order -> transaction list * OrderBook.t
 end
 
+exception UnboundTicker
+
+(** [StringHash] is a module representing a HashedType using String as the key*)
+module StringHash = struct
+  type t = string
+
+  (** [equal i j] is true if i is structurally equal to j and false otherwise *)
+  let equal i j = i = j
+
+  (** [hash i] is i after running a hashing algorithm on it for use in 
+      Hashtbl *)
+  let hash i = Hashtbl.hash i
+end
+
+
+module D = Hashtbl.Make(StringHash)
+
 module MatchingEngine = struct
 
+  type t = {
+    orderbooks: OrderBook.t D.t;
+    account_manager: AccountManager.t;
+  }
+
+  let aapl = OrderBook.empty
+
+  let no_tickers = D.create 20
+
+  let create _ = 
+    let obs = D.create 20 in 
+    D.replace obs "AAPL" OrderBook.empty;
+    D.replace obs "GOOG" OrderBook.empty;
+    D.replace obs "MSFT" OrderBook.empty;
+    D.replace obs "AMZN" OrderBook.empty;
+    D.replace obs "ROKU" OrderBook.empty;
+    let am = AccountManager.create () in 
+    {
+      orderbooks = obs;
+      account_manager = am;
+    }
+
+  let member (me: t) ticker = 
+    let obs = me.orderbooks in D.mem obs ticker
+
+  let tickers (me: t) : string list = 
+    let obs = me.orderbooks in 
+    D.fold (fun ticker ob acc -> ticker :: acc) obs []
+
+  let get_order_book (me: t) (ticker: string) : OrderBook.t = 
+    if member me ticker then 
+      let obs = me.orderbooks in 
+      let ob = D.find obs ticker in 
+      ob
+    else raise UnboundTicker
+
+  let receive_order (me: t) (direction: order_direction) (order: order) 
+      (ticker: string) : unit = 
+    if member me ticker then 
+      let obs = me.orderbooks in 
+      let ob = D.find obs ticker in 
+      let submit_order = (direction, order) in 
+      let ob' = OrderBook.insert_order ob submit_order in 
+      D.replace obs ticker ob'
+    else raise UnboundTicker
+
+  let rec repeat_parse (ob: OrderBook.t) (acc: transaction list) 
+    : (transaction list) * OrderBook.t = 
+    let tx_opt, ob' = OrderBook.construct_tx ob in 
+    match tx_opt with 
+    | Some tx -> 
+      begin
+        let acc' = tx :: acc in 
+        repeat_parse ob' acc'
+      end
+    | None -> acc, ob'
+
+  let parse_order_book (me: t) (ticker: string) 
+    : (transaction list) * OrderBook.t = 
+    let ob = get_order_book me ticker in 
+    repeat_parse ob []
+
+  let update_orderbook (me: t) (ticker: string) (ob: OrderBook.t) : unit = 
+    D.replace me.orderbooks ticker ob 
+
+  let rec process_transactions (am: t) (txs: transaction list) (ticker: string) : unit = 
+    match txs with 
+    | tx :: t -> 
+      let p, amount, addr_b, addr_s = tx in 
+      ()
+    | [] -> ()
+
+  let execute_regular_order (me: t) (direction: order_direction) (order: order) 
+      (ticker: string) : unit = 
+    failwith ""
+
+
+
+  let execute_market_order direction order ticker : unit = failwith ""
+
+
+  (* 
   let matchorder obook order =
     match OrderBook.get_complement_order order obook with 
     | None -> 
@@ -17,5 +114,5 @@ module MatchingEngine = struct
       let transaction = [(order, o)] in 
       let first_removed = (OrderBook.remove order obook) in
       let final_book = OrderBook.remove o first_removed in 
-      transaction, final_book
+      transaction, final_book *)
 end
--- orderBook.mli
+++ orderBook.mli
@@ -1,50 +1,89 @@
-(** The Order Book *)
+type order_direction = Buy | Sell
 
-(** [order_type] is the direction of a single order: Buy or Sell. *)
-type order_type = Buy | Sell
+type address = string
 
-(** [order] is a single order element of the order book that holds the asset,
-    price, order type, and username of the order.  *)
-type order = {
-  asset: string;
-  price: float;
-  order_type: order_type;
-  username: string
-}
+type price = float
 
-(** [OrderBook] is the signature for the module type that will hold all of the 
-    orders in the market. *)
+type amount = int
+
+type time = float
+
+type order = address * int * float * time
+
+type transaction = price * amount * address * address 
+
+type submitted_order = order_direction * order
+
+
 module type OrderBook = sig
-
-  (** [t] is the type of order books. *)
   type t
-
-  (** [empty] is the empty order book. *)
   val empty : t
-
-  (** [is_empty ob] is [true] iff [ob] is empty. *)
   val is_empty : t -> bool
-
-  (** [size ob] is the number of orders in [ob]. *
-      [size empty] is [0]. *)
+  val no_buys : t -> bool
+  val no_sells : t -> bool
   val size : t -> int
+  val insert_order : t -> submitted_order -> t
+  val best_bid : t -> order option
+  val best_offer : t -> order option
+  val pop_best_offer : t -> order option * t
+  val pop_best_bid : t -> order option * t
+  val construct_tx : t -> transaction option * t
+  (* val insert : order -> t -> t
+     val member : order -> t -> bool
+     val get_complement_order : order -> t -> order option
+     val remove : order -> t -> t *)
+end
 
-  (** [insert o ob] is [ob] with order [o] inserted. *)
-  val insert : order -> t -> t
+module OrderBook : OrderBook
 
-  (** [member o ob] is [true] iff order [o] is in [ob]. *)
-  val member : order -> t -> bool
 
-  (** [get_complement_order o] is the order with all fields equal beside an
+(* The Order Book
+
+   (** [order_type] is the direction of a single order: Buy or Sell. *)
+   type order_type = Buy | Sell
+
+   (** [order] is a single order element of the order book that holds the asset,
+    price, order type, and username of the order.  *)
+   type order = {
+   asset: string;
+   price: float;
+   order_type: order_type;
+   username: string
+   }
+
+   (** [OrderBook] is the signature for the module type that will hold all of the 
+    orders in the market. *)
+   module type OrderBook = sig
+
+   (** [t] is the type of order books. *)
+   type t
+
+   (** [empty] is the empty order book. *)
+   val empty : t
+
+   (** [is_empty ob] is [true] iff [ob] is empty. *)
+   val is_empty : t -> bool
+
+   (** [size ob] is the number of orders in [ob]. *
+      [size empty] is [0]. *)
+   val size : t -> int
+
+   (** [insert o ob] is [ob] with order [o] inserted. *)
+   val insert : order -> t -> t
+
+   (** [member o ob] is [true] iff order [o] is in [ob]. *)
+   val member : order -> t -> bool
+
+   (** [get_complement_order o] is the order with all fields equal beside an
       opposite order_type *)
-  val get_complement_order : order -> t -> order option
+   val get_complement_order : order -> t -> order option
 
-  (** [remove o ob] contains all the orders of [ob] except
+   (** [remove o ob] contains all the orders of [ob] except
       order [ob].  If [o] is not in [ob], then
       [remove] returns an order book with the same orders
       as [ob]. *)
-  val remove : order -> t -> t
-end
+   val remove : order -> t -> t
+   end
 
-(** [OrderBook] is the implementation of the [OrderBook] signature *)
-module OrderBook : OrderBook
+   (** [OrderBook] is the implementation of the [OrderBook] signature *)
+   module OrderBook : OrderBook *)
